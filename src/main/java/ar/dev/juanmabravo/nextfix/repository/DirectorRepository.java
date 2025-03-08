9c44c16b0950ec2f5b7db39588bba383a3aae624
juanmas-hub <juanmas-hub@users.noreply.github.com>
2025-03-08 07:00:44 +0000

    deploy: e7f7032d520ef6a7a87ffae75bfd189e69931d3b

--- src/main/java/ar/dev/juanmabravo/nextfix/repository/DirectorRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.dev.juanmabravo.nextfix.repository;
-
-import ar.dev.juanmabravo.nextfix.models.Director;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DirectorRepository extends JpaRepository<Director, Long> {
-}
--- src/main/java/ar/dev/juanmabravo/nextfix/repository/PlataformaRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.dev.juanmabravo.nextfix.repository;
-
-import ar.dev.juanmabravo.nextfix.models.Plataforma;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlataformaRepository extends JpaRepository<Plataforma, Long> { // Clase y tipo de dato del id
-}
